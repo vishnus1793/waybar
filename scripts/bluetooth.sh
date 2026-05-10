16c838419ece77cfbc2df473963dd7ff77385bf6
vishnus1793 <vishnusakthi.44@gmail.com>
2026-05-10 09:40:00 +0530

    Auto commit: 2026-05-10 09:40:00

---
+++ scripts/bluetooth.sh
@@ -0,0 +1,18 @@
+# ----------- BLUETOOTH BATTERY (UPOWER) ----------- #
+BT_CHARGE=$(upower -i "$BT_DEVICE" 2>/dev/null | awk '/percentage/ {print $2}' | tr -d '%')
+
+if [ -z "$BT_CHARGE" ]; then
+    BT_TEXT="🎧 N/A"
+else
+    if [ "$BT_CHARGE" -ge 80 ]; then ICON=""
+    elif [ "$BT_CHARGE" -ge 60 ]; then ICON=""
+    elif [ "$BT_CHARGE" -ge 40 ]; then ICON=""
+    elif [ "$BT_CHARGE" -ge 20 ]; then ICON=""
+    else ICON=""
+    fi
+
+    BT_TEXT="🎧 $ICON $BT_CHARGE%"
+fi
+
+# ----------- OUTPUT ----------- #
+echo "{\"text\": \"$PHONE_TEXT  |  $BT_TEXT\", \"tooltip\": \"Phone: $PHONE_CHARGE% | BT: $BT_CHARGE%\"}"
